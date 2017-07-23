3cda9338bf713f5ed16fecc40d3f5d88f7eac1c6
Marko Devcic <madevcic@gmail.com>
2017-07-23 08:11:14 +0200

    test for no category selected

---
+++ app/src/test/java/com/markodevcic/newsreader/StartupPresenterTest.kt
@@ -0,0 +1,52 @@
+package com.markodevcic.newsreader
+
+import android.content.SharedPreferences
+import com.markodevcic.newsreader.sync.SyncService
+import com.markodevcic.newsreader.util.KEY_CATEGORIES
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class StartupPresenterTest {
+
+	@Mock
+	private lateinit var syncService: SyncService
+	
+	@Mock
+	private lateinit var sharedPreferences: SharedPreferences
+	
+	@Mock
+	private lateinit var editor: SharedPreferences.Editor
+	
+	@Mock
+	private lateinit var startupView: StartupView
+	
+	@InjectMocks
+	private lateinit var sut: StartupPresenter
+
+	@Before
+	fun setup() {
+		MockitoAnnotations.initMocks(this)
+		Mockito.`when`(sharedPreferences.edit()).thenReturn(editor)
+	}
+
+	@Test
+	fun testCategorySaving() {
+		sut.onCategoryChanging("entertainment", true)
+		Mockito.verify(editor)?.putStringSet(Mockito.eq<String>(KEY_CATEGORIES), Mockito.anySet<String>())
+	}
+
+	@Test
+	fun testNoCategorySelected() {
+		Mockito.`when`(sharedPreferences.getStringSet(KEY_CATEGORIES, setOf<String>())).thenReturn(setOf())
+		sut.bind(startupView)
+		runBlocking {
+			sut.downloadAllArticlesAsync()
+		}
+		Mockito.verify(startupView).showNoCategorySelected()
+	}
+}
