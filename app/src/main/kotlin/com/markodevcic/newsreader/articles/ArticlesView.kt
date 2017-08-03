f739bc243ff9ced4ecbf354940d45bb5b0299a0e
Marko Devcic <madevcic@gmail.com>
2017-08-03 22:03:53 +0200

    add calback method

--- app/src/main/kotlin/com/markodevcic/newsreader/articles/ArticlesView.kt
+++ app/src/main/kotlin/com/markodevcic/newsreader/articles/ArticlesView.kt
@@ -3,4 +3,7 @@ package com.markodevcic.newsreader.articles
 import com.markodevcic.newsreader.BaseView
 
 interface ArticlesView : BaseView {
+	fun onUnreadCountChanged(counts: Map<String, Long>)
+
+	fun onNoArticlesSaved()
 }
