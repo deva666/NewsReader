f92b6dd658baac7cf8ca26ee72f292e09f25b06d
Marko Devcic <madevcic@gmail.com>
2017-10-22 16:38:36 +0200

    check if context is activity

--- app/src/main/kotlin/com/markodevcic/newsreader/injection/Injector.kt
+++ app/src/main/kotlin/com/markodevcic/newsreader/injection/Injector.kt
@@ -1,12 +1,18 @@
 package com.markodevcic.newsreader.injection
 
+import android.app.Activity
 import android.content.Context
+import java.lang.IllegalArgumentException
 
 object Injector {
 	private lateinit var context: Context
 	lateinit var appComponent: AppComponent
 
 	fun init(context: Context) {
+		if (context is Activity) {
+			throw IllegalArgumentException("pass an Application as an argument to avoid memory leaks")
+		}
+
 		this.context = context
 		appComponent = DaggerAppComponent.builder()
 				.appModule(AppModule(context))
