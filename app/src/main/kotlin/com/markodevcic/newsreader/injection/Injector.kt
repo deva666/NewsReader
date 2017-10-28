e4e480b5c4a980444579612bc49d3fd3368ae279
Marko Devcic <madevcic@gmail.com>
2017-10-28 18:45:27 +0200

    supress static leak warning

--- app/src/main/kotlin/com/markodevcic/newsreader/injection/Injector.kt
+++ app/src/main/kotlin/com/markodevcic/newsreader/injection/Injector.kt
@@ -1,16 +1,17 @@
 package com.markodevcic.newsreader.injection
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
-import java.lang.IllegalArgumentException
 
+@SuppressLint("StaticFieldLeak")
 object Injector {
 	private lateinit var context: Context
 	lateinit var appComponent: AppComponent
 
 	fun init(context: Context) {
 		if (context is Activity) {
-			throw IllegalArgumentException("pass an Application as an argument to avoid memory leaks")
+			throw IllegalStateException("pass an Application as an argument to avoid memory leaks")
 		}
 
 		this.context = context
