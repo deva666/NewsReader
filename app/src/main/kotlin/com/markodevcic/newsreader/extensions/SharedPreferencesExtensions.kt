7ae9b2c75592fac693476093c84ea637b124d4b3
Marko Devcic <madevcic@gmail.com>
2017-11-01 22:03:58 +0100

    rename method

--- app/src/main/kotlin/com/markodevcic/newsreader/extensions/SharedPreferencesExtensions.kt
+++ app/src/main/kotlin/com/markodevcic/newsreader/extensions/SharedPreferencesExtensions.kt
@@ -2,14 +2,8 @@ package com.markodevcic.newsreader.extensions
 
 import android.content.SharedPreferences
 
-inline fun SharedPreferences.editorApply(action: SharedPreferences.Editor.() -> Unit) {
+inline fun SharedPreferences.apply(action: SharedPreferences.Editor.() -> Unit) {
 	val editor = this.edit()
 	action.invoke(editor)
 	editor.apply()
-}
-
-inline fun SharedPreferences.editorCommit(action: SharedPreferences.Editor.() -> Unit) {
-	val editor = this.edit()
-	action.invoke(editor)
-	editor.commit()
 }
